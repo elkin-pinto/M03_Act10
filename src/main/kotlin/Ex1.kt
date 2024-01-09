ac1a5bf8e82e46d60a8aa15a395277ed5d18f7a2
elkin-pinto <elkin.pinto.7e7@itb.cat>
2024-01-09 09:25:30 +0100

    ex1

--- src/main/kotlin/Ex1.kt
+++ src/main/kotlin/Ex1.kt
@@ -1,7 +1,36 @@
-package org.example
-import java.util.*
-
-
 fun main() {
-    val scan = Scanner(System.`in`)
+    fun m1_iterativo(n: Int): Int {
+        var result = 1
+        for (i in 1..n) {
+            result *= i
+        }
+        return result
+    }
+
+    fun m2_iterativo(x: Double, n: Int): Int {
+        var result = 0
+        for (i in 1..n) {
+            result += i
+        }
+        return result
+    }
+
+    fun m3_iterativo(n: Int): Int {
+        var result = 0
+        for (i in 2 until n) {
+            result += i
+        }
+        return result
+    }
+
+    fun m4_iterativo(n: Int): Int {
+        var result = 0
+        var tempN = n
+        while (tempN != 0) {
+            result += tempN % 10
+            tempN /= 10
+        }
+        return result
+    }
 }
+
--- src/main/kotlin/Ex2.kt
+++ src/main/kotlin/Ex2.kt
@@ -4,4 +4,58 @@ import java.util.*
 
 fun main() {
     val scan = Scanner(System.`in`)
+    println("Di un numero natural positivo")
+    val n1 = leerNumeroNatural(scan,"Ese no es un numero natural, preuba otra vez: ")
+    println("Di un numero natural positivo")
+    val n2 = leerNumeroNatural(scan,"Ese no es un numero natural, preuba otra vez: ")
+
+    val maximoComunDivisor = algoritmeEuclides(n1,n2)
+
+    println(maximoComunDivisor)
+}
+
+/**
+ * Hace le algoritmo de Euclides para poder calcular el maximo comun divisor de dos numeros naturales
+ *
+ * @param n1 es un input de un usuario
+ * @param n2 es un input de un usuario
+ *
+ * @return retorna en última instancia un Int que es el maximo comun divisor.
+ */
+fun algoritmeEuclides(n1:Int,n2:Int):Int {
+    var result:Int
+    var numero1 = n1
+    var numero2 = n2
+    if (numero2 < numero1) {
+        numero1 -= numero2
+        result = algoritmeEuclides(numero1,numero2)
+    }
+    else if (numero1 < numero2) {
+        numero2 -= numero1
+        result = algoritmeEuclides(numero1,numero2)
+    }
+    return result
+}
+
+
+/**
+ * Se asegura que el input del usuario sea un numero natural
+ *
+ * @param scan un scanner
+ * @param err mensaje de error
+ *
+ * @return retorna un Int
+ */
+fun leerNumeroNatural(scan:Scanner, err:String):Int {
+    var input: Int
+    do {
+        while (!scan.hasNextInt()) {
+            scan.nextLine()
+            println(err)
+        }
+        input = scan.nextInt()
+        if (input < 1) println(err)
+    } while(input < 1)
+
+    return input
 }
