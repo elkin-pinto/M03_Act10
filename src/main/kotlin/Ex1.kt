bcc5d3c26b89b11ce10980e6473ef0719b5f7f4f
Omar <omar.zouaoui.7e7@itb.cat>
2024-01-10 13:09:37 +0100

    Documentación de dokka

--- src/main/kotlin/Ex1.kt
+++ src/main/kotlin/Ex1.kt
@@ -1,4 +1,15 @@
+/**
+ * Aquest és un conjunt de funcions per a operacions matemàtiques.
+ * Creador: Omar
+ * Data: 10/1/2024
+ */
 fun main() {
+
+    /**
+     * Calcula el factorial d'un nombre donat.
+     * @param n El nombre d'entrada.
+     * @return El factorial de [n].
+     */
     fun m1_iterativo(n: Int): Int {
         var result = 1
         for (i in 1..n) {
@@ -7,6 +18,12 @@ fun main() {
         return result
     }
 
+    /**
+     * Calcula la suma d'enters de 1 a n.
+     * @param x Paràmetre no utilitzat.
+     * @param n El límit superior per a la suma.
+     * @return La suma d'enters de 1 a [n].
+     */
     fun m2_iterativo(x: Double, n: Int): Int {
         var result = 0
         for (i in 1..n) {
@@ -15,6 +32,11 @@ fun main() {
         return result
     }
 
+    /**
+     * Calcula la suma d'enters de 2 a n-1.
+     * @param n El límit superior per a la suma.
+     * @return La suma d'enters de 2 a [n]-1.
+     */
     fun m3_iterativo(n: Int): Int {
         var result = 0
         for (i in 2 until n) {
@@ -23,6 +45,11 @@ fun main() {
         return result
     }
 
+    /**
+     * Calcula la suma de dígits en un nombre donat.
+     * @param n El nombre d'entrada.
+     * @return La suma de dígits a [n].
+     */
     fun m4_iterativo(n: Int): Int {
         var result = 0
         var tempN = n
@@ -33,4 +60,3 @@ fun main() {
         return result
     }
 }
-
